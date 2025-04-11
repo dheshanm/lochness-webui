2789f659f1287e797f9985669f9ca478f91ec96a
Dheshan Mohandass <dheshan@mohandass.com>
2025-04-11 15:30:37 +0000

    feat.: Add allowedDevOrigins to next.config
    Signed-off-by: Dheshan Mohandass <dheshan@mohandass.com>

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    process.env.HOSTNAME ? `${process.env.HOSTNAME}` : `${process.env.NEXT_PUBLIC_HOSTNAME}`,
+  ],
 };
 
 export default nextConfig;
